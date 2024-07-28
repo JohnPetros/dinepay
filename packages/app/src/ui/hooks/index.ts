09252eb2575ccfe5cf42d7fe6ca0e909d7337d41
JohnPetros <joaopedro.nc@outlook.com>
2024-07-28 12:15:02 -0300

    🖥️ hooks: add useAmountFormatter

---
+++ packages/app/src/ui/hooks/index.ts
@@ -0,0 +1,1 @@
+export { useAmountFormatter } from './use-amount-formatter'
---
+++ packages/app/src/ui/hooks/use-amount-formatter.ts
@@ -0,0 +1,8 @@
+export function useAmountFormatter(amount: number) {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  })
+
+  return formatter.format(amount)
+}
