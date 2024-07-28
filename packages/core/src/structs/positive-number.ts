dd1f10857bc3468a17287216b6eb3bfb81d5a33d
JohnPetros <joaopedro.nc@outlook.com>
2024-07-28 12:15:30 -0300

    🧩 structs: add PositiveNumber

---
+++ packages/core/src/structs/positive-number.ts
@@ -0,0 +1,23 @@
+export class PositiveNumber {
+  private constructor(readonly value: number) {}
+
+  static create(value: number, canBeLessThanZero = true): PositiveNumber {
+    if (!canBeLessThanZero && value <= 0) {
+      throw new Error("Can't be less than or equal to zero")
+    }
+
+    if (value < 0) {
+      throw new Error("Can't be less than zero")
+    }
+
+    return new PositiveNumber(value)
+  }
+
+  isEqualTo(value: number): boolean {
+    return value === this.value
+  }
+
+  get isZero(): boolean {
+    return this.value === 0
+  }
+}
