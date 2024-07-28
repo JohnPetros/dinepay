0bf9b9bbdd7a254cc31bf19d82d7be45d3c0c340
JohnPetros <joaopedro.nc@outlook.com>
2024-07-28 16:15:17 -0300

    ⚙️ config: set sepolia network

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -1,8 +1,18 @@
 import type { HardhatUserConfig } from 'hardhat/config'
+import dotenv from 'dotenv'
 import '@nomicfoundation/hardhat-toolbox'
+
+dotenv.config()
 
 const config: HardhatUserConfig = {
   solidity: '0.8.24',
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.RPC_NODE}`,
+      chainId: Number(process.env.CHAIN_ID),
+      accounts: [String(process.env.ACCOUNT_PRIVATE_KEY)],
+    },
+  },
 }
 
 export default config
